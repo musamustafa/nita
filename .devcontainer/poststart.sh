14949e4b0a95ca04d534899820dfdf33da9b8196
musamustafa <musa19ie@gmail.com>
2024-07-28 22:34:51 +0100

    no minikube

--- .devcontainer/poststart.sh
+++ .devcontainer/poststart.sh
@@ -6,10 +6,9 @@ echo "post-start start"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start start" >> "$HOME/status"
 
  # install kubernetes kind
-#curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
-#chmod +x ./kind
-#mv ./kind /usr/local/bin/kind 
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind 
 
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
 
