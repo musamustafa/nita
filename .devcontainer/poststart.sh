787a77ba84d57003cf437a08a04287efaf3dcc7a
musamustafa <musa19ie@gmail.com>
2024-07-28 15:04:24 +0100

    permission

--- .devcontainer/poststart.sh
+++ .devcontainer/poststart.sh
@@ -8,4 +8,4 @@ echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start start" >> "$HOME/status"
  # install kubernetes kind
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
 chmod +x ./kind
-mv ./kind /usr/local/bin/kind
+mv ./kind /usr/local/bin/kind 
