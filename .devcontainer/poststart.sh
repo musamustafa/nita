e27712d236c5fc3976178c5ddcc84c8daf71f810
musamustafa <musa19ie@gmail.com>
2024-07-28 14:57:14 +0100

    add post start script

---
+++ .devcontainer/poststart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# this runs each time the container starts
+
+echo "post-start start"
+echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start start" >> "$HOME/status"
+
+ # install kubernetes kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind
