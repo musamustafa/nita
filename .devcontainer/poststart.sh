80e148fb5e3f57ae7a5f00b8f1ade79701e77698
musamustafa <musa19ie@gmail.com>
2024-07-28 22:17:31 +0100

    fix minikube

--- .devcontainer/poststart.sh
+++ .devcontainer/poststart.sh
@@ -10,6 +10,6 @@ echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start start" >> "$HOME/status"
 #chmod +x ./kind
 #mv ./kind /usr/local/bin/kind 
 
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-arm64
-sudo install minikube-darwin-arm64 /usr/local/bin/minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
